6d04c6679883b1eebf4707e9ab5fd94db3262989
VillegasMich <manuel.villegas.michel@gmail.com>
2025-05-08 17:42:04 -0500

    feat: hostname of machine in navbar

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template
 from extensions import db, login_manager
 from models.user import User
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://admin:bookstore1213@bookstore.chnlda7rhewa.us-east-1.rds.amazonaws.com/bookstore"
+app.config["SECRET_KEY"] = "secretkey"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+pymysql://admin:bookstore1213@bookstore.chnlda7rhewa.us-east-1.rds.amazonaws.com/bookstore"
+)
 
 db.init_app(app)
 login_manager.init_app(app)
-login_manager.login_view = 'auth.login'
+login_manager.login_view = "auth.login"
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+@app.context_processor
+def inject_hostname():
+    # Ensure hostname is loaded at the very start of the app
+    return {"hostname": os.uname().nodename}
+
 
 # Luego importar blueprints
 from controllers.auth_controller import auth
@@ -24,7 +35,7 @@ from controllers.admin_controller import admin
 
 # Registrar blueprints
 app.register_blueprint(auth)
-app.register_blueprint(book, url_prefix='/book')
+app.register_blueprint(book, url_prefix="/book")
 app.register_blueprint(purchase)
 app.register_blueprint(payment)
 app.register_blueprint(delivery)
@@ -32,25 +43,32 @@ app.register_blueprint(admin)
 
 from models.delivery import DeliveryProvider
 
+
 def initialize_delivery_providers():
     with app.app_context():
         if DeliveryProvider.query.count() == 0:
             providers = [
                 DeliveryProvider(name="DHL", coverage_area="Internacional", cost=50.0),
-                DeliveryProvider(name="FedEx", coverage_area="Internacional", cost=45.0),
+                DeliveryProvider(
+                    name="FedEx", coverage_area="Internacional", cost=45.0
+                ),
                 DeliveryProvider(name="Envia", coverage_area="Nacional", cost=20.0),
-                DeliveryProvider(name="Servientrega", coverage_area="Nacional", cost=15.0),
+                DeliveryProvider(
+                    name="Servientrega", coverage_area="Nacional", cost=15.0
+                ),
             ]
             db.session.bulk_save_objects(providers)
             db.session.commit()
 
-@app.route('/')
-def home():
-    return render_template('home.html')
 
-if __name__ == '__main__':
-# OJO este conexto crea las tablas e inicia los proveedores de entrega, 
-# se debe ejecutar cada que se reinstala y ejecuta la aplicación Bookstore
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    # OJO este conexto crea las tablas e inicia los proveedores de entrega,
+    # se debe ejecutar cada que se reinstala y ejecuta la aplicación Bookstore
     with app.app_context():
         db.create_all()
         initialize_delivery_providers()
